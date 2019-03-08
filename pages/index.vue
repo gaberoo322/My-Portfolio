7cbfde85281552ee93a012cd2c69fc0824f1e6bf
gabe greenfield <gabegreenfield@MacBook-Air.local>
2019-03-07 20:03:35 -0700

    updated the landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,21 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        the amazing display of gabe&#39;s finest software projects
-      </h2>
+      <h1 class="text-grey-darker">I am Gabe Greenfield</h1>
+      <h3 class="text-grey-darker">I like to code the front end</h3>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a href="https://www.linkedin.com/in/gabriel-greenfield-91941579/" target="_blank">
+          <img class="h-12 w-12" src="/svg/linkedin.svg">
+        </a>
+        <a href="https://github.com/gaberoo322" target="_blank">
+          <img class="h-12 w-12" src="/svg/github.svg">
+        </a>
+        <a href="https://twitter.com/GabeGreenfield" target="_blank">
+          <img class="h-12 w-12" src="/svg/twitter.svg">
+        </a>
+        <a href="https://www.facebook.com/gabe.greenfield" target="_blank">
+          <img class="h-12 w-12" src="/svg/facebook.svg">
+        </a>
       </div>
     </div>
   </section>
@@ -34,40 +31,70 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+body {
+  width: 100vh;
+  height: 90vh;
+  color: #fff;
+  background: linear-gradient(
+    -45deg,
+    rgba(99, 193, 50, 1),
+    rgba(184, 225, 255, 1),
+    rgba(161, 205, 244, 1)
+  );
+  background-size: 400% 400%;
+  -webkit-animation: Gradient 15s ease infinite;
+  -moz-animation: Gradient 15s ease infinite;
+  animation: Gradient 15s ease infinite;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  @-webkit-keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 
-.links {
-  padding-top: 15px;
+  @-moz-keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  @keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 }
 </style>
